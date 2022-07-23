dbd67d2cf801996abb494a7aef3ae5e8a00dc9d5
GGoYounghee-ENG <102137580+YoungheeGGO@users.noreply.github.com>
2022-07-24 03:27:28 +0900

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -18,7 +18,7 @@ let render_projects = (slug) => {
             technologies: ['Catboost', 'XAI'],
             description: "Prediction of Social Isolation in the 20s using machine learning and deep learning.",
             categories: ['featured', 'paper']
-        }
+        },
         
  
         
