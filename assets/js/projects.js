2f823a8c31538158c784b6616060654787c0bedc
GGoYounghee-ENG <102137580+YoungheeGGO@users.noreply.github.com>
2022-07-25 15:41:09 +0900

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -54,6 +54,7 @@ let project_mapper = project => {
                         <p class="paragraph-text-normal">${project.description} ${project.demo ? `<a href="${project.demo}">Demo</a>` : ''}</p>
                     </article>
                              
+                    <br/>
                     <div class="card__meta">
                         ${project.technologies.map(tech =>
                             `<span class="project-technology paragraph-text-normal">${tech}</span>`
