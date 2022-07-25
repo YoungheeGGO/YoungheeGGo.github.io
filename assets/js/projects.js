021b63e6641c101c50ad877e4bd50e2bae92a590
GGoYounghee-ENG <102137580+YoungheeGGO@users.noreply.github.com>
2022-07-25 14:17:02 +0900

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -9,55 +9,18 @@ let render_projects = (slug) => {
     $('.white-button').removeClass('white-button-hover');
     $(`#${slug}`).addClass('white-button-hover');
 
-    let projects_obj = [
+        let projects_obj = [
         {
-            image: 'assets/images/gagu.png',
-            link: 'https://github.com/Jeong-Byeong-hun/gagubook',
-            title: 'Gagubook',
-            demo: false,
-            technologies: ['Android', 'Retrofit2', 'Java'],
-            description: "DB에 등록된 가구 중 매장이 판매하고 있는 제품의 이미지와 정보 API로 받아서 태블릿으로 고객들에게 정보를 보여주는 앱",
-            categories: ['featured', 'java']
-        },
-        {
-            image: 'assets/images/remotejobs.png',
-            link: 'https://repl.it/@JeongByeonghun/Day-Thirteen-and-Fourteen#main.py',
-            title: 'Jobs Scrap',
-            demo: false,
-            technologies: ['Python', 'Flask', 'Beautifulsoup'],
-            description: "stackoverflow,  weworkremotely,  remotreok 세 개의 사이트에서 구인글을 스크랩해서 보여주고 csv파일로 다운로드 할 수 있는 사이트",
-	 categories: ['featured', 'python']
-        },
-        {
-            image: 'assets/images/newsSearch.png',
-            link: 'https://github.com/Jeong-Byeong-hun/newsSearch',
-            title: '3줄 뉴스',
-            demo: 'https://github.com/abhn/Marvel',
-            technologies: ['Android', 'Beautifulsoup', 'Python'],
-            description: "단어를 검색하면 관련된 네이버API를 이용해 뉴스를 스크랩한 후 TextRank 알고리즘을 이용해 3줄 요약을 볼 수 있는 앱",
-            categories: ['java', 'python']
-        },
-        {
-            image: 'assets/images/jjayo.png',
-            link: 'https://github.com/leolyoo/Jiayou',
-            title: 'Jjayo',
-            demo: false,
-            technologies: ['Android', 'Glide', 'Java'],
-            description: "중국어과 학생들을 위해 시간이 날 때 마다 중국어를 공부 할 수 있게 간단한 퀴즈와 따라 그리기를 넣은 앱",
-            categories: ['java']
-        },
-		{
-            image: 'assets/images/kotlin.png',
-            link: 'https://github.com/Jeong-Byeong-hun/KotlinBasic',
-            title: 'Kotlin Basic',
-            demo: false,
-            technologies: ['Android', 'Kotlin'],
-            description: "코틀린 책을 보면서 책 내용에서 바꿔 만든 기초 앱",
-            categories: ['kotlin']
+            image: 'assets/images/statistics-Jornal-cut.jpg',
+            link: 'https://github.com/YoungheeGGO/Social_Isolation_for_20s',
+            title: 'Social Isolation for 20\'s',
+            demo: false
+            technologies: ['Catboost', 'XAI'],
+            description: "Prediction of Social Isolation in the 20s using machine learning and deep learning.",
+            categories: ['featured', 'paper']
         }
-       
-      
-       
+ 
+        
     ]
 
     let projects = [];
