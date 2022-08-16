3e0cf1cdbf3ea13701036563a93f23f111dec566
GGoYounghee-ENG <102137580+YoungheeGGO@users.noreply.github.com>
2022-08-16 18:32:56 +0900

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -60,7 +60,7 @@ let render_projects = (slug) => {
             {
                 image: 'assets/images/yongin.png',
                 link: 'https://github.com/YoungheeGGO/Yong-In-Si-Bigdata-Competition',
-                title: 'Local business analysis',
+                title: 'Hot-spot Analysis for Business Location',
                 /*demo: false*/
                 technologies: ['GMM'],
                 description: "Derivation of measures to support young entrepreneurs through regional commercial district analysis in YongIn-si,South Korea",
