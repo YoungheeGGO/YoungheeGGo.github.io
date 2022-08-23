b55211913c2f75b1b15039667951ff743e1aab94
GGoYounghee-ENG <102137580+YoungheeGGO@users.noreply.github.com>
2022-08-23 21:47:04 +0900

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -12,7 +12,7 @@ let render_projects = (slug) => {
         let projects_obj = [
             
             {
-                image: 'assets/images/coming.PNG',
+                image: 'assets/images/coming_.png',
                 link: 'https://github.com/YoungheeGGO/Second-Hand-price',
                 title: 'Second Hand Price',
                 /*demo: false*/
